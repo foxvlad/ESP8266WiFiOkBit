6d2ff90fda23b7762d451256b7cbe67bd83cbb90
SmarHome <foxvlad@yandex.ru>
2018-10-31 11:17:46 +0300

    ESPClimat

--- OkbitUDP_ESP8266/examples/ESPClimat/ESPClimat.ino
+++ OkbitUDP_ESP8266/examples/ESPClimat/ESPClimat.ino
@@ -5,11 +5,23 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <EEPROM.h>
-#include <Bounce.h>
 #include <OkbitUDP_ESP8266.h>
+#include "DHT.h"
+
+#define DHTPIN D1     // what digital pin we're connected to
+
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11   // DHT 11
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+
+long last_mls = millis();
+
+int time_period = 10000; //период опроса температуры и влажности
 
 const char* ssid = "   ";
-const char* password = "  ";
+const char* password = "    ";
 
 unsigned int localUdpPort = 6400;  // локальный порт для прослушки
 char incomingPacket[100];          // буфер для входящих пакетов
@@ -17,37 +29,20 @@ char replyPacekt[100];  // буфер для исходящих пакетов
 
 int sub_id = 0; //адресс подсети
 int id = 0; //адрес устройства
-int device = 7001; //тип устройства смотрим в модуле
-int firmware[] = {0,9}; //версия прошивки
+int device = 7002; //тип устройства смотрим в модуле
+int firmware[] = {0, 9}; //версия прошивки
 
-
-const int lamp1 = D6; // выход управления лампой 1
-const int lamp2 = D7; // выход управления лампой 2
-const int lamp3 = D8; // выход управления лампой 3
-
-const int in1 = D1; // вход управления лампой 1
-const int in2 = D2; // вход управления лампой 2
-const int in3 = D3; // вход управления лампой 3
-
-Bounce bouncer1 = Bounce( in1, 10 ); // подавление дребезга контактов 1-го входа
-Bounce bouncer2 = Bounce( in2, 10 ); // подавление дребезга контактов 2-го входа
-Bounce bouncer3 = Bounce( in3, 10 ); // подавление дребезга контактов 3-го входа
 
 WiFiUDP Udp;
 OKBIT_UDP HandlerUDP(sub_id, id, device, firmware[0], firmware[1]);//подключение класса обработчика протокола OkBIT UDP
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
 
   Serial.begin(115200);
   Serial.println();
 
-  pinMode(lamp1, OUTPUT); // выход управления лампой 1
-  pinMode(lamp2, OUTPUT); // выход управления лампой 2
-  pinMode(lamp3, OUTPUT); // выход управления лампой 3
 
-  pinMode(in1, INPUT); // вход управления лампой 1
-  pinMode(in2, INPUT); // вход управления лампой 2
-  pinMode(in3, INPUT); // вход управления лампой 2
 
   Serial.printf("Connecting to %s ", ssid); //  "Подключение к %s "
   WiFi.begin(ssid, password);
@@ -63,6 +58,7 @@ void setup() {
 
   EEPROM.begin(512);
   HandlerUDP.eeprom_read();
+  dht.begin();
 
 }
 
@@ -74,7 +70,7 @@ void loop()
     // получаем входящие UDP-пакеты:
     Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort()); //  "Получено %d байт от %s, порт %d%"
     int len = Udp.read(incomingPacket, 255);
-    if (len > 0){
+    if (len > 0) {
       incomingPacket[len] = 0;
     }
     Serial.printf("UDP packet contents: %s\n", incomingPacket);
@@ -100,43 +96,18 @@ void loop()
     Udp.endPacket();
   }
 
-  debounce(); // вызов функции обработки нажатий кнопок
-
-  digitalWrite(lamp1, HandlerUDP.holdingRegs[1]); // вкл\выкл - лампы в зависимости от пришедшей команды по UDP или по нажатию кнопки
-  digitalWrite(lamp2, HandlerUDP.holdingRegs[2]);
-  digitalWrite(lamp3, HandlerUDP.holdingRegs[3]); 
+  if (millis() - last_mls > time_period) {
+    temperatura();
+    last_mls = millis();
+  }
 }
 
 
-void debounce() { //Обработчик нажатия кнопок
+void  temperatura(){
 
-  if ( bouncer1.update() ) {
-    if ( bouncer1.read() == LOW) {
-      if ( HandlerUDP.holdingRegs[1] == LOW ) {
-        HandlerUDP.holdingRegs[1] = HIGH;
-      } else {
-        HandlerUDP.holdingRegs[1] = LOW;
-      }
-    }
-  }
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
 
-  if ( bouncer2.update() ) {
-    if ( bouncer2.read() == LOW) {
-      if ( HandlerUDP.holdingRegs[2] == LOW ) {
-        HandlerUDP.holdingRegs[2] = HIGH;
-      } else {
-        HandlerUDP.holdingRegs[2] = LOW;
-      }
-    }
-  }
-
-  if ( bouncer3.update() ) {
-    if ( bouncer3.read() == LOW) {
-      if ( HandlerUDP.holdingRegs[3] == LOW ) {
-        HandlerUDP.holdingRegs[3] = HIGH;
-      } else {
-        HandlerUDP.holdingRegs[3] = LOW;
-      }
-    }
-  }
+  HandlerUDP.holdingRegs[1]  = t * 100;
+  HandlerUDP.holdingRegs[2]  = h * 100;
 }
